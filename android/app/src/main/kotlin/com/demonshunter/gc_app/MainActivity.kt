483dd97bf9948c6b436cd79137820a326384fce3
othy1010 <othmane.elkarmy@um5r.ac.ma>
2022-05-31 20:36:50 +0100

    hello

--- android/app/src/main/kotlin/com/demonshunter/gc_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/demonshunter/gc_app/MainActivity.kt
@@ -2,5 +2,5 @@ package com.demonshunter.gc_app
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterActivity() {
 }
--- android/app/src/main/kotlin/com/example/gc_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/gc_app/MainActivity.kt
@@ -2,5 +2,5 @@ package com.example.gc_app
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterActivity() {
 }
